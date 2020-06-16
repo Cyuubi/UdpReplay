95d3bb55bb84fc5d05d3ade5e3257600826d3c33
Natalie C <cyuubiapps@gmail.com>
2020-06-15 22:52:35 -0400

    Clean up, handle invalid/missed packets semi-correctly?

--- UdpReplay/Program.cs
+++ UdpReplay/Program.cs
@@ -2,7 +2,6 @@
 using SharpPcap;
 using SharpPcap.LibPcap;
 using PacketDotNet;
-using System.Linq;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
@@ -17,30 +16,26 @@ namespace UdpReplay
         private class PacketInfo
         {
             public byte[] Data;
-            public bool IsServer;
 
-            public PacketInfo(byte[] data, bool isServer)
-            {
-                Data = data;
-                IsServer = isServer;
-            }
+            public bool IsServer;
+            public bool IsValid;
         }
 
-        private static string _clientHardwareAddress = string.Empty;
+        private static Dictionary<int, PacketInfo> _packets = new Dictionary<int, PacketInfo>();
+
+        private static string _clientMacAddress = string.Empty;
 
         private static int _packetIndex = 0;
-
-        private static Dictionary<int, PacketInfo> _packets = new Dictionary<int, PacketInfo>();
 
         static void Main(string[] args)
         {
             if (args == null || args.Length <= 2)
             {
-                Console.WriteLine($"{Process.GetCurrentProcess().ProcessName}.exe <Capture File> <Client Hardware Address> <Server Port>");
+                Console.WriteLine($"{Process.GetCurrentProcess().ProcessName}.exe <Capture> <Client MAC Address> <Port>");
                 return;
             }
 
-            _clientHardwareAddress = args[1].Replace(":", string.Empty).ToUpper();
+            _clientMacAddress = args[1].Replace(":", string.Empty).ToUpper();
 
             ICaptureDevice device;
 
@@ -51,7 +46,7 @@ namespace UdpReplay
             }
             catch (Exception e)
             {
-                Console.WriteLine($"An error occured while opening capture file: {e.ToString()}");
+                Console.WriteLine($"An error occured while opening capture: {e}");
                 return;
             }
 
@@ -60,99 +55,141 @@ namespace UdpReplay
             device.Capture();
             device.Close();
 
-            Console.WriteLine("Reading capture file finished, press any key to start server!");
-            Console.ReadKey();
-
             _packetIndex = 0;
 
-            byte[] buffer = new byte[32768];
+            Console.WriteLine("Reading capture finished, press any key to start server!");
+            Console.ReadKey();
 
-            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, Convert.ToInt32(args[2]));
-            UdpClient udpClient = new UdpClient(endPoint);
+            var buffer = new byte[0];
+            var server = new UdpClient(new IPEndPoint(IPAddress.Any, Convert.ToInt32(args[2])));
+            var sender = new IPEndPoint(IPAddress.Any, 0);
 
             Console.WriteLine($"Server has started on port {args[2]}.");
 
-            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-
-            buffer = udpClient.Receive(ref sender);
-
-            _packetIndex++;
-
-            PushPackets(udpClient, sender);
-
             while (true)
             {
-                buffer = udpClient.Receive(ref sender);
+                buffer = server.Receive(ref sender);
 
-                _packetIndex++;
-
-                PushPackets(udpClient, sender);
+                PushPackets(server, sender);
             }
         }
 
-        private static void PushPackets(UdpClient udpClient, IPEndPoint sender)
+        private static string EnumerableByteToString(IEnumerable<byte> enumerable)
         {
-            for (int index = _packetIndex; index < _packets.Count - 1; index++)
-            {
-                PacketInfo packetInfo = _packets[index];
+            var builder = new StringBuilder("byte[] { ");
 
+            foreach (var b in enumerable)
+                builder.Append($"0x{b:X2}, ");
+
+            builder.Append("}");
+
+            return builder.ToString();
+        }
+
+        private static void PushPackets(UdpClient server, IPEndPoint sender)
+        {
+            _packetIndex++;
+
+            for (var index = _packetIndex; index < _packets.Count - 1; index++)
+            {
+                var packetInfo = _packets[index];
                 if (packetInfo.IsServer)
                 {
-                    udpClient.Send(packetInfo.Data, packetInfo.Data.Length, sender);
+                    // TODO: Make configurable?
+                    if (packetInfo.IsValid)
+                    {
+                        server.Send(packetInfo.Data, packetInfo.Data.Length, sender);
 
-                    _packetIndex = index;
+                        _packetIndex = index;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Packet {_packetIndex} was not valid, attempting to send previous valid packet...");
+
+                        var foundValid = false; // TODO: Add later.
+
+                        for (var _index = _packetIndex; _index < _packets.Count - 1; _index--)
+                        {
+                            var _packetInfo = _packets[_index];
+                            if (_packetInfo.IsServer)
+                            {
+                                if (_packetInfo.IsValid)
+                                {
+                                    foundValid = true;
+
+                                    Console.WriteLine($"Packet {_index} was valid, sent packet.");
+
+                                    server.Send(packetInfo.Data, packetInfo.Data.Length, sender);
+
+                                    break;
+                                }
+                            }
+                        }
+
+                        _packetIndex = index;
+                    }
                 }
 
                 // If the next packet is from the client, stop the for loop.
                 // Otherwise, continue the for loop so that we can send the next server packet.
-                if (!_packets[index + 1].IsServer) break;
+                if (!_packets[index + 1].IsServer)
+                    break;
             }
+        }
+
+        private static byte[] RemoveTrailing(byte[] value)
+        {
+            var index = value.Length - 1;
+
+            while (value[index] == 0)
+                --index;
+
+            var result = new byte[index + 1];
+            Array.Copy(value, result, index + 1);
+
+            return result;
         }
 
         private static void OnPacketArrival(object sender, CaptureEventArgs e)
         {
             if (e.Packet.LinkLayerType == LinkLayers.Ethernet)
             {
-                Packet packet = Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
-                EthernetPacket ethernetPacket = (EthernetPacket)packet;
+                var packet = Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+                var ethernet = (EthernetPacket)packet;
 
-                if (ethernetPacket.PayloadPacket.PayloadPacket.HasPayloadData)
+                if (ethernet.PayloadPacket.PayloadPacket.HasPayloadData)
                 {
+                    var isValid = true;
+
                     using (MemoryStream stream = new MemoryStream(e.Packet.Data))
                     using (BinaryReader reader = new BinaryReader(stream))
                     {
                         stream.Seek(0x26, SeekOrigin.Begin);
 
-                        byte[] _length = reader.ReadBytes(2);
+                        var _length = reader.ReadBytes(2);
                         Array.Reverse(_length);
 
-                        ushort length = BitConverter.ToUInt16(_length, 0);
-                        if (length != ethernetPacket.PayloadPacket.PayloadPacket.PayloadData.Length + 8)
+                        var length = BitConverter.ToUInt16(_length, 0);
+                        if (length != ethernet.PayloadPacket.PayloadPacket.PayloadData.Length + 8)
                         {
                             Console.WriteLine($"Packet {_packetIndex} has failed length check, this packet will be ignored.");
-                            Console.WriteLine($"    Expected Length = {length}, Actual Length = {ethernetPacket.PayloadPacket.PayloadPacket.PayloadData.Length}");
+                            Console.WriteLine($"    Expected Length = {length}, Actual Length = {ethernet.PayloadPacket.PayloadPacket.PayloadData.Length}");
 
-                            return;
+                            isValid = false;
                         }
                     }
 
-                    _packets.Add(_packetIndex, new PacketInfo(RemoveTrailingZeros(ethernetPacket.PayloadPacket.PayloadPacket.PayloadData), ethernetPacket.SourceHardwareAddress.ToString() != _clientHardwareAddress));
+                    _packets.Add(_packetIndex, new PacketInfo()
+                    {
+                        Data = RemoveTrailing(ethernet.PayloadPacket.PayloadPacket.PayloadData),
+
+                        IsServer = ethernet.SourceHardwareAddress.ToString() != _clientMacAddress,
+                        IsValid = isValid
+                    });
 
                     _packetIndex++;
                 }
             }
-        }
-
-        private static byte[] RemoveTrailingZeros(byte[] value)
-        {
-            int index = value.Length - 1;
-
-            while (value[index] == 0) --index;
-
-            byte[] result = new byte[index + 1];
-            Array.Copy(value, result, index + 1);
-
-            return result;
         }
     }
 }
